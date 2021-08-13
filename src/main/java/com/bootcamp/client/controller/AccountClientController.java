b8eefcff6a94393c3037cc6549089cf20a0b21b1
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-12 19:23:06 -0500

    feat: refactor controllers response body and add dockerfile and config

--- src/main/java/com/bootcamp/client/controller/AccountClientController.java
+++ src/main/java/com/bootcamp/client/controller/AccountClientController.java
@@ -1,6 +1,8 @@
 package com.bootcamp.client.controller;
 
 import com.bootcamp.client.bean.RequestClientAccountBean;
+import com.bootcamp.client.dto.BaseResponseDto;
+import com.bootcamp.client.model.AccountClientEntity;
 import com.bootcamp.client.service.AccountClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,44 +11,36 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/account/p-customer")
+@RequestMapping("/p-customers/accounts")
 public class AccountClientController
 {
     @Autowired
     private AccountClientService accountClientService;
 
     @PostMapping("/{documentNumber}")
-    public Mono<ResponseEntity<Map<String, Object>>> createAccountClient(@Valid @RequestBody Mono<RequestClientAccountBean> bean, @PathVariable String documentNumber){
+    public Mono<ResponseEntity<BaseResponseDto<AccountClientEntity>>> createAccountClient(@Valid @RequestBody Mono<RequestClientAccountBean> bean, @PathVariable String documentNumber){
 
         return  bean.flatMap(requestClientAccountBean -> {
-            Map<String, Object> response = new HashMap<>();
-
             return accountClientService.createClientAccount(requestClientAccountBean, documentNumber)
                 .flatMap(accountClientEntity -> {
-                    response.put("status", HttpStatus.OK.value());
-                    response.put("message", "Se asigno al cliente");
-                    response.put("body", accountClientEntity);
-
-                    return Mono.just(ResponseEntity.ok().body(response));
+                    BaseResponseDto<AccountClientEntity> response = new BaseResponseDto<>(HttpStatus.CREATED, "Account created for client with document number: " + documentNumber, accountClientEntity);
+                    return Mono.just(ResponseEntity.created(URI.create("/p-customers/accounts")).body(response));
                 });
         }).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{accountNumber}/{documentNumber}")
-    public Mono<ResponseEntity<Map<String, Object>>> getAccountClient(@PathVariable String accountNumber,
+    public Mono<ResponseEntity<BaseResponseDto<AccountClientEntity>>> getAccountClient(@PathVariable String accountNumber,
                                                                       @PathVariable String documentNumber){
-        Map<String, Object> response = new HashMap<>();
         return accountClientService.getAccountClient(accountNumber, documentNumber)
                 .flatMap(accountClientEntity -> {
-                    response.put("status", HttpStatus.OK.value());
-                    response.put("message", "Se muestra la cuenta");
-                    response.put("body", accountClientEntity);
-
-                    return Mono.just(ResponseEntity.ok().body(response));
+                    BaseResponseDto<AccountClientEntity> response = new BaseResponseDto<AccountClientEntity>(HttpStatus.FOUND, "Account founded", accountClientEntity);
+                    return Mono.just(ResponseEntity.status(HttpStatus.FOUND).body(response));
                 })
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
--- src/main/java/com/bootcamp/client/controller/ClientController.java
+++ src/main/java/com/bootcamp/client/controller/ClientController.java
@@ -1,8 +1,10 @@
 package com.bootcamp.client.controller;
 
 import com.bootcamp.client.bean.RequestBean;
+import com.bootcamp.client.dto.BaseResponseDto;
 import com.bootcamp.client.model.ClientEntity;
 import com.bootcamp.client.service.ClientService;
+import com.ctc.wstx.shaded.msv_core.util.Uri;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,74 +14,59 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/p-customer")
+@RequestMapping("/p-customers")
 @Slf4j
-public class ClientController
-{
+public class ClientController {
     @Autowired
     private ClientService clientService;
 
     @GetMapping("/all")
-    public Flux<ClientEntity> getClients(){
+    public Flux<ClientEntity> getClients() {
         return clientService.getClient();
     }
 
     @PostMapping("")
-    public Mono<ResponseEntity<Map<String, Object>>> createClient(@Valid @RequestBody Mono<RequestBean> bean) throws Exception {
-        Map<String, Object> response = new HashMap<>();
-
+    public Mono<ResponseEntity<BaseResponseDto<ClientEntity>>> createClient(@Valid @RequestBody Mono<RequestBean> bean) throws Exception {
         return clientService.createClient(bean)
-                    .flatMap(clientEntity -> {
-                        response.put("status", HttpStatus.OK.value());
-                        response.put("message", "Se creo el cliente");
-                        response.put("body", clientEntity);
-
-                        return Mono.just(ResponseEntity.ok().body(response));
-                    });
+                .flatMap(clientEntity -> {
+                    BaseResponseDto<ClientEntity> response = new BaseResponseDto<ClientEntity>(HttpStatus.CREATED, "Client created", clientEntity);
+                    return Mono.just(ResponseEntity.created(URI.create("/p-customers")).body(response));
+                });
     }
 
     @GetMapping("")
-    public Mono<ResponseEntity<Map<String, Object>>> getClient(@RequestParam String documentNumber){
-        Map<String, Object> response = new HashMap<>();
+    public Mono<ResponseEntity<BaseResponseDto<ClientEntity>>> getClient(@RequestParam String documentNumber) {
         return clientService.getClient(documentNumber)
                 .flatMap(clientEntity -> {
-                    response.put("status", HttpStatus.OK.value());
-                    response.put("message", "Se obtiene la información del cliente");
-                    response.put("body", clientEntity);
-
-                    return Mono.just(ResponseEntity.ok().body(response));
+                    BaseResponseDto<ClientEntity> response = new BaseResponseDto<ClientEntity>(HttpStatus.FOUND, "Client founded", clientEntity);
+                    return Mono.just(ResponseEntity.status(HttpStatus.FOUND).body(response));
                 })
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("")
-    public Mono<ResponseEntity<Map<String, Object>>> deleteClient(@RequestParam String documentNumber){
-        Map<String, Object> response = new HashMap();
+    public Mono<ResponseEntity<BaseResponseDto<Void>>> deleteClient(@RequestParam String documentNumber) {
         return clientService.deleteClient(documentNumber)
                 .map(clientEntity -> {
-                    response.put("status", HttpStatus.OK.value());
-                    response.put("message", "Se eliminó el cliente");
-
-                    return ResponseEntity.ok().body(response);
+                    BaseResponseDto<Void> responseDto = new BaseResponseDto<Void>(HttpStatus.ACCEPTED, "Client deleted");
+                    return ResponseEntity.accepted().body(responseDto);
                 });
     }
 
     @PutMapping("")
-    public Mono<ResponseEntity<Map<String, Object>>> updateClient(@Valid @RequestBody RequestBean bean,
-                                                                  @RequestParam String documentNumber){
-        Map<String, Object> response = new HashMap<>();
+    public Mono<ResponseEntity<BaseResponseDto<ClientEntity>>> updateClient(@Valid @RequestBody RequestBean bean,
+                                                                            @RequestParam String documentNumber) {
         return clientService.updateClient(bean, documentNumber)
                 .flatMap(clientEntity -> {
-                    response.put("status", HttpStatus.OK);
-                    response.put("message", "Cliente actualizado");
-                    response.put("body", clientEntity);
-
-                    return Mono.just(ResponseEntity.ok().body(response));
+                    BaseResponseDto<ClientEntity> response = new BaseResponseDto<ClientEntity>(HttpStatus.ACCEPTED, "Client has been updated", clientEntity);
+                    return Mono.just(ResponseEntity.accepted().body(response));
                 })
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
